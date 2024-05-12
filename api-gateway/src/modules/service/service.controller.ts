6a8c5eb1332e95397579874a43eb8ef45e1455e0
Umar  Khodjaev <125152511+Omarkhodjaev@users.noreply.github.com>
2024-05-12 17:47:10 +0500

    redis added to service

--- api-gateway/src/modules/service/service.controller.ts
+++ api-gateway/src/modules/service/service.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, UseInterceptors } from '@nestjs/common';
 import { ServiceService } from './service.service';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
+import { RedisKeys } from 'src/common/types/enums';
 
 @ApiTags('service')
 @Controller('service')
@@ -12,7 +14,9 @@ export class ServiceController {
   create(@Body() createServiceDto: CreateServiceDto) {
     return this.serviceService.create(createServiceDto);
   }
-
+  @UseInterceptors(CacheInterceptor)
+  @CacheKey(RedisKeys.ALL_SERVICES)
+  @CacheTTL(0)
   @Get()
   findAll() {
     return this.serviceService.findAll();
--- api-gateway/src/modules/service/service.service.ts
+++ api-gateway/src/modules/service/service.service.ts
@@ -3,18 +3,26 @@ import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
 import { PARK_PACKAGE, USER_PACKAGE } from 'src/common/const/microservices';
 import { ClientGrpc } from '@nestjs/microservices';
+import { Cache } from '@nestjs/cache-manager';
+import { RedisKeys } from 'src/common/types/enums';
 
 @Injectable()
 export class ServiceService {
   private serviceService: any;
 
-  constructor(@Inject(PARK_PACKAGE) private Parkclient: ClientGrpc) {}
+  constructor(
+    @Inject(PARK_PACKAGE) private Parkclient: ClientGrpc,
+
+    @Inject('CACHE_MANAGER')
+    private cacheManager: Cache,
+  ) {}
 
   onModuleInit() {
     this.serviceService = this.Parkclient.getService('ServiceService');
   }
 
   async create(createServiceDto: CreateServiceDto) {
+    await this.deleteDataInRedis(RedisKeys.ALL_SERVICES);
     return await this.serviceService.create(createServiceDto);
   }
 
@@ -27,6 +35,12 @@ export class ServiceService {
   }
 
   async remove(id: number) {
+    await this.deleteDataInRedis(RedisKeys.ALL_SERVICES);
+
     return await this.serviceService.remove({ id });
+  }
+
+  private async deleteDataInRedis(key: RedisKeys) {
+    await this.cacheManager.del(key);
   }
 }
