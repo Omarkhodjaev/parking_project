3a296a815e115233c0ad2fd0edd26aedfb8ca53e
Umar  Khodjaev <125152511+Omarkhodjaev@users.noreply.github.com>
2024-05-12 15:01:01 +0500

    userTariff added auht

--- api-gateway/src/modules/user-tariff/user-tariff.controller.ts
+++ api-gateway/src/modules/user-tariff/user-tariff.controller.ts
@@ -6,33 +6,43 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UserTariffService } from './user-tariff.service';
 import { CreateUserTariffDto } from './dto/create-user-tariff.dto';
 import { UpdateUserTariffDto } from './dto/update-user-tariff.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../shared/guards/auth/jwt-auth.guard';
+import { RolesGuard } from '../shared/guards/role.guard';
 
 @ApiTags('user-tariff')
 @Controller('user-tariff')
 export class UserTariffController {
   constructor(private readonly userTariffService: UserTariffService) {}
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Post()
   create(@Body() createUserTariffDto: CreateUserTariffDto) {
-    
     return this.userTariffService.create(createUserTariffDto);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get()
   findAll() {
     return this.userTariffService.findAll();
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userTariffService.findOneById(+id);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -41,6 +51,8 @@ export class UserTariffController {
     return this.userTariffService.update(+id, updateUserTariffDto);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userTariffService.delete(+id);
