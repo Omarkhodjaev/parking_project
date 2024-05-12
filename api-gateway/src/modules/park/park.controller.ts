3993a8a47f80b5010486da70403a82ed917376d0
Umar  Khodjaev <125152511+Omarkhodjaev@users.noreply.github.com>
2024-05-12 17:54:55 +0500

    redis added to parks

--- api-gateway/src/modules/park/park.controller.ts
+++ api-gateway/src/modules/park/park.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ParkService } from './park.service';
 import { CreateParkDto } from './dto/create-park.dto';
 import { UpdateParkDto } from './dto/update-park.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
+import { RedisKeys } from 'src/common/types/enums';
 
 @ApiTags('park-service')
 @Controller('park')
@@ -22,6 +25,9 @@ export class ParkController {
     return this.parkService.create(createParkDto);
   }
 
+  @UseInterceptors(CacheInterceptor)
+  @CacheKey(RedisKeys.ALL_PARKS)
+  @CacheTTL(0)
   @Get()
   findAll() {
     return this.parkService.findAll();
@@ -33,7 +39,10 @@ export class ParkController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateParkDto: Partial<UpdateParkDto>) {
+  update(
+    @Param('id') id: string,
+    @Body() updateParkDto: Partial<UpdateParkDto>,
+  ) {
     return this.parkService.update(+id, updateParkDto);
   }
 
--- api-gateway/src/modules/park/park.service.ts
+++ api-gateway/src/modules/park/park.service.ts
@@ -3,19 +3,25 @@ import { CreateParkDto } from './dto/create-park.dto';
 import { UpdateParkDto } from './dto/update-park.dto';
 import { ClientGrpc } from '@nestjs/microservices';
 import { PARK_PACKAGE } from 'src/common/const/microservices';
-import { lastValueFrom } from 'rxjs';
+import { RedisKeys } from 'src/common/types/enums';
+import { Cache } from '@nestjs/cache-manager';
 
 @Injectable()
 export class ParkService {
   private userService: any;
 
-  constructor(@Inject(PARK_PACKAGE) private Parkclient: ClientGrpc) {}
+  constructor(
+    @Inject(PARK_PACKAGE) private Parkclient: ClientGrpc,
+    @Inject('CACHE_MANAGER')
+    private cacheManager: Cache,
+  ) {}
 
   onModuleInit() {
     this.userService = this.Parkclient.getService('ParkService');
   }
 
   async create(createParkDto: CreateParkDto) {
+    await this.deleteDataInRedis(RedisKeys.ALL_PARKS);
     return await this.userService.create(createParkDto);
   }
 
@@ -28,10 +34,16 @@ export class ParkService {
   }
 
   async update(id: number, updateParkDto: Partial<UpdateParkDto>) {
+    await this.deleteDataInRedis(RedisKeys.ALL_PARKS);
     return await this.userService.update({ id, dto: updateParkDto });
   }
 
   async remove(id: number) {
+    await this.deleteDataInRedis(RedisKeys.ALL_PARKS);
     return await this.userService.remove({ id });
+  }
+
+  private async deleteDataInRedis(key: RedisKeys) {
+    await this.cacheManager.del(key);
   }
 }
