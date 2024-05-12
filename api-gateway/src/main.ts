5d61c5e53fd07c04116d8155835b42f1ba982d18
Umar  Khodjaev <125152511+Omarkhodjaev@users.noreply.github.com>
2024-05-12 23:05:09 +0500

    kodni qaytardik

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors();
+  console.log(1);
+  
 
   app.useBodyParser('json');
 
