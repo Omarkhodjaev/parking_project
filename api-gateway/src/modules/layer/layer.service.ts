d8f52dd5afaad8eeef99a000f07fa475b6b76a76
Umar  Khodjaev <125152511+Omarkhodjaev@users.noreply.github.com>
2024-05-12 21:19:42 +0500

    redis layers

--- api-gateway/src/modules/layer/layer.service.ts
+++ api-gateway/src/modules/layer/layer.service.ts
@@ -3,18 +3,26 @@ import { CreateLayerDto } from './dto/create-layer.dto';
 import { UpdateLayerDto } from './dto/update-layer.dto';
 import { ClientGrpc } from '@nestjs/microservices';
 import { PARK_PACKAGE } from 'src/common/const/microservices';
+import { Cache } from '@nestjs/cache-manager';
+import { RedisKeys } from 'src/common/types/enums';
 
 @Injectable()
 export class LayerService {
   private layerService: any;
 
-  constructor(@Inject(PARK_PACKAGE) private Parkclient: ClientGrpc) {}
+  constructor(
+    @Inject(PARK_PACKAGE) private Parkclient: ClientGrpc,
+    @Inject('CACHE_MANAGER')
+    private cacheManager: Cache,
+  ) {}
 
   onModuleInit() {
     this.layerService = this.Parkclient.getService('LayerService');
   }
 
   async create(createLayerDto: CreateLayerDto) {
+    await this.deleteDataInRedis(RedisKeys.ALL_LAYERS);
+
     return await this.layerService.create(createLayerDto);
   }
 
@@ -27,10 +35,17 @@ export class LayerService {
   }
 
   async update(id: number, dto: UpdateLayerDto) {
+    await this.deleteDataInRedis(RedisKeys.ALL_LAYERS);
+
     return await this.layerService.update({ id, dto });
   }
 
   async remove(id: number) {
+    await this.deleteDataInRedis(RedisKeys.ALL_LAYERS);
     return await this.layerService.remove({ id });
+  }
+
+  async deleteDataInRedis(key: string) {
+    await this.cacheManager.del(key);
   }
 }
